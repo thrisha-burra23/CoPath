ba9ded30ffc8910ceea92de57b2d7a9fbe1f45cb
Thrisha Burra <thrishagoud023@gmail.com>
2026-01-13 19:44:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+
+
+
+function App() {
+  return <>
+  {/* <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+      </Routes>
+    </BrowserRouter> */}
+
+    <Login/>
+
+  </>;
+}
+
+export default App;
